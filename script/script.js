fefd49d5fe847c44a8ce1423ec6a9fd1c0d50d92
MukunaChristian <you@example.com>
2024-03-14 14:20:34 +0200

    added change

--- script/script.js
+++ script/script.js
@@ -1,17 +1,17 @@
-const API_URL = 'https://picsum.photos/1920/1080'; // Lorem Picsum API for random images
+// const API_URL = 'https://picsum.photos/1920/1080'; // Lorem Picsum API for random images
 
-// Fetch random background image from the Lorem Picsum API
-fetch(API_URL)
-.then(response => {
-    const hero = document.getElementById('hero');
-    const heroText = document.getElementById('hero-text');
+// // Fetch random background image from the Lorem Picsum API
+// fetch(API_URL)
+// .then(response => {
+//     const hero = document.getElementById('hero');
+//     const heroText = document.getElementById('hero-text');
 
-    // Set background image
-    hero.style.backgroundImage = `url('${response.url}')`;
+//     // Set background image
+//     hero.style.backgroundImage = `url('${response.url}')`;
 
-    // Update hero text with placeholder text
-    heroText.textContent = "Empowering businesses with robust web hosting and IT solutions that propel their success in the digital world";
-})
-.catch(error => {
-    console.error('Error fetching background image:', error);
-});
+//     // Update hero text with placeholder text
+//     heroText.textContent = "Empowering businesses with robust web hosting and IT solutions that propel their success in the digital world";
+// })
+// .catch(error => {
+//     console.error('Error fetching background image:', error);
+// });
